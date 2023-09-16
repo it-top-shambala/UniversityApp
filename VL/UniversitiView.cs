56dbf2bf06b4288663efef6478ffb9d5b26fb36d
Starinin Andrey <starininandrey@gmail.com>
2023-09-16 13:52:47 +0300

    Добавление уровня представления

---
+++ VL/UniversitiView.cs
@@ -0,0 +1,23 @@
+﻿using UniversityApp.Models;
+
+namespace UniversityApp.VL;
+
+public static class UniversityView
+{
+    #region Teachers
+
+    public static void ShowTeacher(Teacher teacher)
+    {
+        CLI.PrintInfo($"{teacher.Id}: {teacher.FullName}, {teacher.Faculty}, {teacher.Age}, {teacher.Sex}");
+    }
+
+    public static void ShowTeachers(List<Teacher> teachers)
+    {
+        foreach (var teacher in teachers)
+        {
+            ShowTeacher(teacher);
+        }
+    }
+
+    #endregion
+}
