a5f89f45063cd80764601dac529cfa3bdf9e5eec
Starinin Andrey <starininandrey@gmail.com>
2023-09-16 13:20:48 +0300

    Добавлен абстрактный класс FileContext

---
+++ DAL/FileContext.cs
@@ -0,0 +1,13 @@
+﻿namespace UniversityApp.DAL;
+
+public abstract class FileContext
+{
+    protected readonly string PathToTeachers;
+    protected readonly string PathToStudents;
+
+    protected FileContext(string pathToTeachers, string pathToStudents)
+    {
+        PathToTeachers = pathToTeachers;
+        PathToStudents = pathToStudents;
+    }
+}
