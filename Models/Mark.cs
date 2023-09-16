fdf473190869cf7abf07b8a942f5af677747295b
Starinin Andrey <starininandrey@gmail.com>
2023-09-16 11:43:44 +0300

    Добавление класса Mark

---
+++ Models/Mark.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityApp.Models;
+
+public record Mark
+{
+    public string Id { get; init; }
+    public DateTime Date { get; init; }
+    public string Subject { get; init; }
+    public Teacher Teacher { get; init; }
+    public int Rating { get; init; }
+}
