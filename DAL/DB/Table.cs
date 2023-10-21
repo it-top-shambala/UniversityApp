56c04d4b511328d1ff1f8c9dcadb300d31f4e867
Starinin Andrey <starininandrey@gmail.com>
2023-10-21 13:16:27 +0300

    Вынесение общего функционала в абстрактный класс Table

---
+++ DAL/DB/Table.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace UniversityApp.DAL.DB;
+
+public abstract class Table<T>
+{
+    private readonly NpgsqlConnection _db;
+
+    protected Table(string connectionString)
+    {
+        _db = new NpgsqlConnection(connectionString);
+
+        Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+    }
+
+    protected bool ExecuteNonQuery(string sql)
+    {
+        _db.Open();
+        var result = _db.Execute(sql);
+        _db.Close();
+
+        return result > 0;
+    }
+
+    protected IEnumerable<T> ExecuteQuery(string sql)
+    {
+        _db.Open();
+        var result = _db.Query<T>(sql);
+        _db.Close();
+
+        return result;
+    }
+    
+    protected T? ExecuteQuerySingle(string sql)
+    {
+        _db.Open();
+        var result = _db.QuerySingleOrDefault<T>(sql);
+        _db.Close();
+
+        return result;
+    }
+}
---
+++ DAL/DB/TablePersons.cs
@@ -0,0 +1,48 @@
+﻿using UniversityApp.DAL.DB.Model;
+
+namespace UniversityApp.DAL.DB;
+
+public class TablePersons : Table<Person>, ICrud<Person>
+{
+    public TablePersons(string connectionString) : base(connectionString)
+    {
+        
+    }
+    
+    
+    public bool Insert(Person obj)
+    {
+        var sql = $"""
+                   INSERT INTO table_persons(last_name, first_name, patronymic, date_of_birth)
+                   VALUES ('{obj.LastName}', '{obj.FirstName}', '{obj.Patronymic}', '{obj.DateOfBirth:yyyy-MM-dd}')
+                   """;
+        return ExecuteNonQuery(sql);
+    }
+
+    public bool Update(Person obj)
+    {
+        var sql = $"""
+                   UPDATE table_persons
+                   SET last_name = '{obj.LastName}', first_name = '{obj.FirstName}', patronymic = '{obj.Patronymic}', date_of_birth = '{obj.DateOfBirth:yyyy-MM-dd}'
+                   WHERE id = {obj.Id};
+                   """;
+        return ExecuteNonQuery(sql);
+    }
+
+    public bool Delete(Person obj)
+    {
+        return false;
+    }
+
+    public IEnumerable<Person> GetAll()
+    { 
+        var sql = "SELECT * FROM table_persons";
+        return ExecuteQuery(sql);
+    }
+
+    public Person? GetById(int id)
+    {
+        var sql = $"SELECT * FROM table_persons WHERE id = {id}";
+        return ExecuteQuerySingle(sql);
+    }
+}
