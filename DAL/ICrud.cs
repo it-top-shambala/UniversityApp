a0313e387cb3195dd1bb602806cbe6d58954a761
Starinin Andrey <starininandrey@gmail.com>
2023-10-21 13:14:26 +0300

    Убрал поддержку null  в методе GetAll()

--- DAL/ICrud.cs
+++ DAL/ICrud.cs
@@ -5,6 +5,6 @@ public interface ICrud<T>
     public bool Insert(T obj);
     public bool Update(T obj);
     public bool Delete(T obj);
-    public IEnumerable<T>? GetAll();
+    public IEnumerable<T> GetAll();
     public T? GetById(int id);
 }
